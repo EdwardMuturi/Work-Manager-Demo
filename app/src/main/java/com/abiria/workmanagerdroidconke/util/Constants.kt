fd2fe8f8242503b96f8867bd725abcf27563707b
Edward Muturi <edward@Edwards-MacBook-Pro.local>
2019-08-02 14:35:01 +0300

    add api key to gradle.properties file, add gradle.properties file to gitignore file

--- app/src/main/java/com/abiria/workmanagerdroidconke/util/Constants.kt
+++ app/src/main/java/com/abiria/workmanagerdroidconke/util/Constants.kt
@@ -1,10 +1,14 @@
 package com.abiria.workmanagerdroidconke.util
 
+import com.abiria.workmanagerdroidconke.BuildConfig
+
 /**
  * Created by Edward Muturi on 2019-08-02.
  */
 class Constants {
     companion object{
-        val API_TOKEN= "My token goes here"
+        val API_KEY= BuildConfig.API_KEY
+        val TMDB_LANGUAGE= "en-US"
+        val TMDB_PAGE= "1"
     }
 }
