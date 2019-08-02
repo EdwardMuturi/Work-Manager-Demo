0a120b86ad6da6b713d2cf6461220cb76df6f3b5
Edward Muturi <edward@Edwards-MacBook-Pro.local>
2019-08-02 15:16:39 +0300

    implement call to get popuplar movies from api

--- app/src/main/java/com/abiria/workmanagerdroidconke/api/MovieService.kt
+++ app/src/main/java/com/abiria/workmanagerdroidconke/api/MovieService.kt
@@ -1,26 +1,37 @@
 package com.abiria.workmanagerdroidconke.api
 
+import androidx.lifecycle.LiveData
+import com.abiria.workmanagerdroidconke.model.PopularMovie
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 /**
  * Created by Edward Muturi on 2019-08-02.
  */
 
-private const val BASE_URL= "https://api.themoviedb.org/3/movie/popular"
+private const val BASE_URL = "https://api.themoviedb.org/3/"
 
 interface MovieService {
-    companion object{
-        val getMovieService : MovieService
-        get() {
-            val retrofit= Retrofit.Builder()
-                .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl(BASE_URL)
-                .build()
+    @GET("movie/popular")
+    fun getPopularMovies(
+        @Query("api_key") api_key: String,
+        @Query("language") language: String,
+        @Query("page") page: String
+    ): Call<PopularMovie>
 
-            return retrofit.create(MovieService::class.java)
-        }
+    companion object {
+        val getMovieService: MovieService
+            get() {
+                val retrofit = Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl(BASE_URL)
+                    .build()
+
+                return retrofit.create(MovieService::class.java)
+            }
     }
 
 }
--- app/src/main/java/com/abiria/workmanagerdroidconke/view/activity/MainActivity.kt
+++ app/src/main/java/com/abiria/workmanagerdroidconke/view/activity/MainActivity.kt
@@ -1,14 +1,51 @@
 package com.abiria.workmanagerdroidconke.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 import com.abiria.workmanagerdroidconke.R
+import com.abiria.workmanagerdroidconke.api.MovieService
+import com.abiria.workmanagerdroidconke.model.PopularMovie
+import com.abiria.workmanagerdroidconke.util.Constants.Companion.API_KEY
+import com.abiria.workmanagerdroidconke.util.Constants.Companion.TMDB_LANGUAGE
+import com.abiria.workmanagerdroidconke.util.Constants.Companion.TMDB_PAGE
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var popularMovieJson : String
+    private val TAG= MainActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fetchPopularMovie()
     }
+
+    fun fetchPopularMovie(){
+        MovieService.getMovieService.getPopularMovies(API_KEY, TMDB_LANGUAGE, TMDB_PAGE).enqueue(object:
+            Callback<PopularMovie>{
+            override fun onFailure(call: Call<PopularMovie>, t: Throwable) {
+                Log.d(TAG, t.message.toString())
+            }
+
+            override fun onResponse(call: Call<PopularMovie>, response: Response<PopularMovie>) {
+                if (response.isSuccessful){
+                    response.body()?.let {
+                        popularMovieJson= response.body().toString()
+                        Log.d(TAG, popularMovieJson)
+                    }
+
+                }
+            }
+
+        })
+    }
+
+
 }
